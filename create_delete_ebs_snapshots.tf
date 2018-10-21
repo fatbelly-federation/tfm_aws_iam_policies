127af2f3c0e6816bffe36070636de699eb113fc3
GPanula <github@kablah.com>
2018-10-21 17:16:45 -0500

    tighten up the create ebs snapshot policy

--- create_delete_ebs_snapshots.tf
+++ create_delete_ebs_snapshots.tf
@@ -14,14 +14,16 @@ data "aws_iam_policy_document" "create_delete_ebs_snapshots" {
     # sid *must* be alphanumberic
     sid       = "20181021"
     effect    = "Allow"
-    resources = [ "*" ]
+    resources = [ 
+                "arn:aws:ec2:*::snapshot/*",
+                "arn:aws:ec2:*:*:volume/*"
+    ]
     actions   = [ 
                 "ec2:CreateSnapshot",
                 "ec2:CreateTags",
                 "ec2:DeleteSnapshot",
                 "ec2:DescribeSnapshots",
-                "ec2:DescribeVolumes",
-                "ec2:DescribeInstances"
+                "ec2:DescribeVolumes"
     ]
     # enforce encrypted communications
     condition = {
@@ -30,5 +32,20 @@ data "aws_iam_policy_document" "create_delete_ebs_snapshots" {
         values    = ["true"]
       }
   }
+
+    statement {
+    # sid *must* be alphanumberic
+    sid       = "20181021"
+    effect    = "Allow"
+    resources = [ "*" ]
+    actions   = [ "ec2:DescribeInstances" ]
+    # enforce encrypted communications
+    condition = {
+        test      = "Bool"
+        variable  = "aws:SecureTransport"
+        values    = ["true"]
+      }
+  }
+
 }
 
